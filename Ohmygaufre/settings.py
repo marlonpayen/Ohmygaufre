dd73f3e22dbc63640f86223c54b45b241627872b
mpayen <marlon.payen4@gmail.com>
2024-04-14 12:48:36 -0400

    Deployment

--- Ohmygaufre/settings.py
+++ Ohmygaufre/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 #COMMENT FOR TESTING
 DEBUG = False
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com']
 
 # UNCOMMENT FOR TESTING
 #DEBUG = True
