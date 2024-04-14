8a0536cba70a70b6a2076e68de7fdff8b676d3b2
mpayen <marlon.payen4@gmail.com>
2024-04-14 19:43:03 -0400

    Deploy

--- website/views.py
+++ website/views.py
@@ -58,9 +58,7 @@ def send_message(request):
     return render(request, template.render(context, request), {'form': form})
   
 def my_custom_page_not_found_view(request, *args, **argv):
-    response = render('404.html', {}, context_instance=RequestContext(request))
-    response.status_code = 404
-    return response
+    return render(request, '400.html')
 
 def my_custom_error_view(request, *args, **argv):
     return render(request, '500.html')
