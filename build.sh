4fec1648a863e1fcc960819c52ed87984f722d33
mpayen <marlon.payen4@gmail.com>
2024-04-14 20:16:59 -0400

    d

--- build.sh
+++ build.sh
@@ -11,4 +11,7 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-python manage.py createsuperuser2 --username tomrectem --password tom
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
