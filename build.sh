0705e6425777f44d27ded001d9b7f6498f9aefe3
mpayen <marlon.payen4@gmail.com>
2024-04-14 20:13:47 -0400

    Deploy

--- build.sh
+++ build.sh
@@ -11,7 +11,4 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input
-fi
+python manage.py createsuperuser2 --username tomrectem --password tom
